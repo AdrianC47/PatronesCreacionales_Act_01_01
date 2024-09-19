d23d38efd4ea3652cfceb8a4ce1aba03509b085e
Edisson Redrovan <eredrovan@baustro.fin.ec>
2024-09-19 15:21:20 -0500

    Se elimina clase no en uso

--- Escenario01/src/main/java/ec/edu/ups/controller/VehiculoController.java
+++ Escenario01/src/main/java/ec/edu/ups/controller/VehiculoController.java
@@ -8,7 +8,6 @@ import ec.edu.ups.model.VehiculoFactory;
 import java.util.ArrayList;
 import java.util.List;
 import ec.edu.ups.model.Vehiculo;
-import ec.edu.ups.view.VehiculoView;
 
 /**
  *
@@ -17,38 +16,15 @@ import ec.edu.ups.view.VehiculoView;
 public class VehiculoController {
     
     private VehiculoFactory vehiculoFactory;
-    private VehiculoView vista;
-    
-    private List<Vehiculo> vehiculos;
 
-//    public VehiculoController(VehiculoFactory vehiculoFactory, VehiculoView vista) {
-//        this.vehiculoFactory = vehiculoFactory;
-//        this.vista = vista;
-//        this.vehiculos = new ArrayList<>();
-//    }
+    private List<Vehiculo> vehiculos;
 
     public VehiculoController(VehiculoFactory vehiculoFactory) {
         this.vehiculoFactory = vehiculoFactory;
         this.vehiculos = new ArrayList<>();
         
     }
- 
 
-    public void registrarVehiculo() {
-        String tipo = vista.obtenerTipoVehiculo();
-        String placa = vista.obtenerPlaca();
-        String marca = vista.obtenerMarca();
-        String modelo = vista.obtenerModelo();
-        int año = vista.obtenerAño();
-
-        Vehiculo vehiculo = vehiculoFactory.crearVehiculo(tipo, placa, marca, modelo, año);
-        vehiculos.add(vehiculo);
-        vista.mostrarCostoMatrícula(vehiculo);
-    }
-   
-    public void mostrarVehiculos() {
-        vista.mostrarListaVehiculos(vehiculos);
-    }
     
     public double registrarVehiculo(String tipo, String placa, String marca, String modelo, int año) {
         Vehiculo vehiculo = vehiculoFactory.crearVehiculo(tipo, placa, marca, modelo, año);
